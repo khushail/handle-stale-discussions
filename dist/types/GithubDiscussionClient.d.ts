f3b2b45cca7c9c3796ddbd206afbd0a786ab4c4b
peterwoodworth <woodwoop@amazon.com>
2023-05-26 20:16:31 -0700

    refactor client

---
+++ dist/types/GithubDiscussionClient.d.ts
@@ -0,0 +1,24 @@
+import { ApolloClient, NormalizedCacheObject } from "@apollo/client/core";
+import { DiscussionConnection } from "@octokit/graphql-schema";
+import { MarkDiscussionCommentAsAnswerMutation, AddLabelToDiscussionMutation, UpdateDiscussionCommentMutation, ReactionContent } from "./generated/graphql";
+export declare class GithubDiscussionClient {
+    private githubToken;
+    private owner;
+    private repo;
+    private attentionLabelId;
+    constructor(owner: string, repo: string);
+    get githubClient(): ApolloClient<NormalizedCacheObject>;
+    closeDiscussionsInAbsenceOfReaction(commentDate: Date, discussionId: string): Promise<void>;
+    triggerReactionContentBasedAction(content: ReactionContent, bodyText: string, discussionId: string, commentId: string, proposedAnswerText: string): Promise<void>;
+    remindAuthorForAction(commentDate: Date, author: string, discussionId: string, remindResponseText: string): Promise<void>;
+    getTotalDiscussionCount(categoryID: string): Promise<number | undefined>;
+    getDiscussionsMetaData(categoryID: string): Promise<DiscussionConnection>;
+    getAnswerableDiscussionCategoryIDs(): Promise<any>;
+    getAttentionLabelId(label: string): Promise<string>;
+    closeDiscussionAsResolved(discussionId: string): Promise<string | undefined>;
+    closeDiscussionAsOutdated(discussionId: string): Promise<string | undefined>;
+    addCommentToDiscussion(discussionId: string, body: string): Promise<void>;
+    markDiscussionCommentAsAnswer(commentId: string): Promise<import("@apollo/client/core").FetchResult<MarkDiscussionCommentAsAnswerMutation>>;
+    addAttentionLabelToDiscussion(discussionId: string): Promise<import("@apollo/client/core").FetchResult<AddLabelToDiscussionMutation>>;
+    updateDiscussionComment(commentId: string, body: string): Promise<import("@apollo/client/core").FetchResult<UpdateDiscussionCommentMutation>>;
+}
